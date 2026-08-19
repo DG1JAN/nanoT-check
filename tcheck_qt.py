14492c93cec10f5204536915f775a1547dc4a4ea
Jan <jan.liebhaeuser@gmx.de>
2026-08-19 22:43:20 +0200

    Add files via upload

--- tcheck_qt.py
+++ tcheck_qt.py
@@ -683,11 +683,15 @@ class TCheckWindow(QMainWindow):
         layout.addWidget(self._canvas)
 
     def _style_axes(self):
+        # plot text scaled up from the form label font size
+        fs = self._lbl_port.font().pointSize()
+        if fs <= 0:
+            fs = 9
         ax = self._ax
         ax.set_facecolor(PLOT_BG)
-        ax.set_xlabel(self._tr("axis_freq"), color=FG, fontsize=10)
-        ax.set_ylabel(self._tr("axis_ct"),   color=FG, fontsize=10)
-        ax.tick_params(colors=FG, which="both", labelsize=10)
+        ax.set_xlabel(self._tr("axis_freq"), color=FG, fontsize=fs + 6)
+        ax.set_ylabel(self._tr("axis_ct"),   color=FG, fontsize=fs + 6)
+        ax.tick_params(colors=FG, which="both", labelsize=fs + 4)
         for spine in ax.spines.values():
             spine.set_edgecolor(FG_DIM)
 
